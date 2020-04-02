f8e9ad425bc27228e738dc2a153425ba80f8b4d6
mcnerd16 <mcnerd16@gmail.com>
2020-04-01 21:06:59 -0700

    Updated F, with enable and logic to manage enable

--- Sources/F.sv
+++ Sources/F.sv
@@ -22,7 +22,7 @@
 
 module F #(parameter CWIDTH = 320, parameter XWORDS32 = 9, parameter DS_WIDTH = 4,
             parameter RWIDTH = 32, parameter ROUND_COUNT = 10, parameter IWIDTH = 128) (
-    input logic clk, reset,
+    input logic clk, reset, en,
     input logic [CWIDTH-1:0] c,
     input logic [XWORDS32*32-1:0] x,
     input logic [IWIDTH-1:0] i,
@@ -36,8 +36,19 @@ module F #(parameter CWIDTH = 320, parameter XWORDS32 = 9, parameter DS_WIDTH = 
     
     logic [CWIDTH-1:0] cReg;
     logic mixDone;
-    
+    logic enMix, enG;
     assign xout = x;
+    
+    always_comb begin
+        enMix = 1'b0;
+        enG = 1'b0;
+        if (en) begin
+            enMix = 1'b1;
+            if (mixDone) begin
+                enG = 1'b1;
+            end
+        end
+    end
     
     Mix128 #(.CWIDTH(CWIDTH), .XWORDS32(XWORDS32), .DS_WIDTH(DS_WIDTH)) mix (
         .c(c),
@@ -46,6 +57,7 @@ module F #(parameter CWIDTH = 320, parameter XWORDS32 = 9, parameter DS_WIDTH = 
         .ds(ds),
         .clk(clk),
         .reset(reset),
+        .en(enMix),
         .done(mixDone),
         .cout(cReg)
     );
@@ -56,6 +68,7 @@ module F #(parameter CWIDTH = 320, parameter XWORDS32 = 9, parameter DS_WIDTH = 
         .cout(cout),
         .clk(clk),
         .reset(reset | ~mixDone),
+        .en(enG),
         .done(done),
         .rounds(rounds)
     );
